b60856bac0fba3140443234c198ec67af2d0341e
matesp3 <mpoljak3@gmail.com>
2025-03-05 11:58:11 +0100

    simulation core

--- src/mpoljak/dsim/intro/Main.java
+++ src/mpoljak/dsim/intro/Main.java
@@ -10,18 +10,10 @@ public class Main {
         double d = 5;
         double l = 4;
         MCSimCore mc = new BuffonNeedle(new Random(), repCount, l, d ); // Monte carlo simulation
-        double prob = mc.run(repCount);
+        mc.simulate();
+        double prob = mc.getResult();
         double pi = (2 * l) / (d * prob);
         System.out.println("Probability of crossing: "+prob);
         System.out.println("Replications: "+repCount+"  | result PI: "+pi);
-
-        Random rnd = new Random();
-        double minVal = 10;
-        double maxVal = 70;
-        double percent = rnd.nextDouble() * (maxVal - minVal) + minVal;
-        if (rnd.nextDouble()*100 <= percent)
-            System.out.println("dodavatel doda suciastky");
-        else
-            System.out.println("dodavatel NEdoda suciastky");
     }
 }
