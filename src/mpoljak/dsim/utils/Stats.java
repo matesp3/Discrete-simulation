e6986f6a1b46a10987bce887fb264e9d021db0b1
matesp3 <mpoljak3@gmail.com>
2025-03-29 17:47:17 +0100

    SW02 - stats ok

--- src/mpoljak/dsim/utils/Stats.java
+++ src/mpoljak/dsim/utils/Stats.java
@@ -3,6 +3,8 @@ package mpoljak.dsim.utils;
 import mpoljak.dsim.common.Generator;
 import mpoljak.dsim.generators.DiscreteUniformRnd;
 
+import java.util.Arrays;
+
 public abstract class Stats {
     protected double sum;
     protected double count;
@@ -47,11 +49,11 @@ public abstract class Stats {
             this.samplesCount++;
         }
 
-    @Override
-    public String toString() {
-        return "ArithmeticAvg "+ super.toString();
+       @Override
+        public String toString() {
+            return "ArithmeticAvg "+ super.toString();
+        }
     }
-}
 //  -   -   -   -   -   -   -   ARI AVG -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
     public static class WeightedAvg extends Stats {
         /**
@@ -65,15 +67,15 @@ public abstract class Stats {
             this.samplesCount++;
         }
 
-    @Override
-    public String toString() {
-        return "WeightedAvg "+ super.toString();
-    }
+        @Override
+        public String toString() {
+            return "WeightedAvg "+ super.toString();
+        }
     }
 //  -   -   -   -   -   -   -   C I -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-    public abstract static class ConfidenceInterval {
-        protected static final double T_ALFA = 1.96 ; // from 30 samples
-        protected double sumOfSquares;
+    public static class ConfidenceInterval extends ArithmeticAvg {
+        private static final double T_ALFA = 1.96 ; // from 30 samples
+        private double sumOfSquares;
 
         public ConfidenceInterval() {
             super();
@@ -81,37 +83,36 @@ public abstract class Stats {
         }
 
         /**
+         * Adds new sample to statistics from which is confidence interval computed.
+         * @param value
+         */
+        @Override
+        public void addSample(double value) {
+            super.addSample(value);
+            this.sumOfSquares += Math.pow(value, 2);
+        }
+
+    /**
          * @return standard deviation
          */
         public double getStdDev() {
-                return Math.pow(this.getVariance(), 0.5);
+                return Math.sqrt(this.getVariance());
         }
 
-        public abstract double getVariance();
 
-        /**
-         * @return half-width of 95% confidence interval.
-         */
-        public abstract double getHalfWidthCI();
-        /**
-         * Computes 95% confidence interval from current state of this instance.
-         * @return [0] - lower bound, [1] upper bound of 95% confidence interval
-         */
-        public abstract double[] getCI();
-
-        protected double getVariance(Stats stats) {
-            return (this.sumOfSquares - Math.pow(stats.getSum(), 2)/ stats.getCount()) / (stats.getCount() - 1);
+        public double getVariance() {
+            return (this.sumOfSquares - Math.pow(this.sum, 2)/ this.count) / (this.count - 1);
         }
 
         /**
          * Computes 95% confidence interval from current state of this instance.
          * @return [0] - lower bound, [1] upper bound of 95% confidence interval
          */
-        protected double[] getCI(Stats stats) {
-            if (stats.getCount() < 30)
-                throw new RuntimeException("Cannot compute confidence interval for only "+stats.getCount()+ " samples."
+        public double[] getCI() {
+            if (this.count < 30)
+                throw new RuntimeException("Cannot compute confidence interval for only "+this.count+ " samples."
                         + " There must be at least 30 samples.");
-            double mean = stats.getMean();
+            double mean = this.getMean();
             double h = this.getHalfWidthCI();
             return new double[]{mean - h, mean + h};
         }
@@ -119,11 +120,11 @@ public abstract class Stats {
         /**
          * @return half-width of 95% confidence interval.
          */
-        protected double getHalfWidthCI(Stats stats) {
-            if (stats.getCount() < 30)
-                throw new RuntimeException("Cannot compute confidence interval for only "+stats.getCount()+ " samples." +
+        protected double getHalfWidthCI() {
+            if (this.count < 30)
+                throw new RuntimeException("Cannot compute confidence interval for only "+this.count+ " samples." +
                         " There must be at least 30 samples.");
-            return (this.getStdDev() * T_ALFA) / Math.sqrt(stats.getCount());
+            return (this.getStdDev() * T_ALFA) / Math.sqrt(this.count);
         }
 
         @Override
@@ -133,87 +134,27 @@ public abstract class Stats {
         }
     }
 
-//  -   -   -   -   -   -   -   C I  f o r  A AVG-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-    public static class ArithmeticCI extends ConfidenceInterval {
-        private ArithmeticAvg stats;
-
-        public ArithmeticCI(ArithmeticAvg stats) {
-            super();
-            if (stats == null)
-                throw new IllegalArgumentException("Stats not provided");
-            this.stats = stats;
-        }
-
-        /**
-         * Adds new sample to statistics from which is confidence interval computed.
-         * @param value
-         */
-        public void addSample(double value) {
-            this.stats.addSample(value);
-            this.sumOfSquares += Math.pow(value, 2);
-        }
-
-        @Override
-        public double getVariance() {
-            return super.getVariance(this.stats);
-        }
-
-        @Override
-        public double getHalfWidthCI() {
-            return super.getHalfWidthCI(this.stats);
-        }
-
-        @Override
-        public double[] getCI() {
-            return super.getCI(this.stats);
-        }
-    }
-
-//  -   -   -   -   -   -   -   C I  f o r  W AVG-   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-    public static class WeightedCI extends ConfidenceInterval {
-        private WeightedAvg stats;
-
-        public WeightedCI(WeightedAvg stats) {
-            super();
-            if (stats == null)
-                throw new IllegalArgumentException("Stats not provided");
-            this.stats = stats;
-        }
-
-        public void addSample(double value, double weight) {
-            this.stats.addSample(value, weight);
-            this.sumOfSquares += Math.pow(value * weight, 2);
-        }
-
-        @Override
-        public double getVariance() {
-            return super.getVariance(this.stats);
-        }
-
-        @Override
-        public double getHalfWidthCI() {
-            return super.getHalfWidthCI(this.stats);
-        }
-
-        @Override
-        public double[] getCI() {
-            return super.getCI(this.stats);
-        }
-    }
-
     public static void main(String[] args) {
         Stats.ArithmeticAvg avgStats = new ArithmeticAvg();
         Stats.WeightedAvg wavgStats = new WeightedAvg();
+        Stats.ConfidenceInterval ci = new ConfidenceInterval();
+
         Generator rndS = new DiscreteUniformRnd(120,789);
         Generator rndW = new DiscreteUniformRnd(1,10);
+
         for (int i = 0; i < 1000; i++) {
             double sample = rndS.sample();
             double weight = rndW.sample();
             avgStats.addSample(sample);
             wavgStats.addSample(sample, weight);
+            ci.addSample(sample);
         }
+
         System.out.println(avgStats);
         System.out.println(wavgStats);
+        System.out.println(ci);
+        double[] interval = ci.getCI();
+        System.out.printf("Confidence interval: <%.03f   |%.03f|   %.03f>", interval[0], ci.getMean(), interval[1]);
     }
 
 }
