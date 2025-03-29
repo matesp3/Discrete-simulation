5abf2f9bfbf64fedad845ff281fbe81e818f617a
matesp3 <mpoljak3@gmail.com>
2025-03-29 12:13:09 +0100

    SW02 - desks allocation

--- src/mpoljak/dsim/assignment_02/logic/sim/FurnitureStore.java
+++ src/mpoljak/dsim/assignment_02/logic/sim/FurnitureStore.java
@@ -7,13 +7,12 @@ import mpoljak.dsim.generators.ContinuosUniformRnd;
 import mpoljak.dsim.generators.ExponentialRnd;
 import mpoljak.dsim.generators.TriangularRnd;
 
+import java.rmi.UnexpectedException;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class FurnitureStore extends EventSim {
-    private Queue<DiscreteEvent> queueA;
-    private Queue<DiscreteEvent> queueB;
-    private Queue<DiscreteEvent> queueC;
     // generators
     private final Generator rndOrderArrival;
     private final Generator rndOrderType;
@@ -33,6 +32,11 @@ public class FurnitureStore extends EventSim {
     private final Generator rndStainingWardrobe;
     private final Generator rndAssemblingWardrobe;
     private final Generator rndFitInstallWardrobe;
+    // other logic
+    private final Queue<DiscreteEvent> queueA;
+    private final Queue<DiscreteEvent> queueB;
+    private final Queue<DiscreteEvent> queueC;
+    private final DeskAllocation deskManager;
 
     public FurnitureStore(long replicationsCount, int amountA, int amountB, int amountC) {
         super(replicationsCount, 15, 358_560); // 60*24*249 = 358_560 [min]
@@ -59,27 +63,76 @@ public class FurnitureStore extends EventSim {
         this.queueA = new ConcurrentLinkedQueue<>(); // https://docs.oracle.com/javase/6/docs/api/java/util/concurrent/ConcurrentLinkedQueue.html
         this.queueB = new ConcurrentLinkedQueue<>(); // FIFO ordering based on the docs --^
         this.queueC = new ConcurrentLinkedQueue<>(); // methods of interest: add & poll
+        this.deskManager = new DeskAllocation(amountA+amountB+amountC); // sum is enough - maximum occupancy
+    }
+
+    @Override
+    protected void beforeExperiment() {
+        super.beforeExperiment();
+        this.deskManager.freeAllDesks();
+    }
+
+    private static class DeskAllocation {
+        private final String[] desks;
+        private int firstFree;
+
+        public DeskAllocation(int amountOfDesks) {
+            this.desks = new String[amountOfDesks];
+            this.firstFree = 0;
+        }
+
+        public boolean isAnyDeskAvailable() {
+            return this.firstFree > -1;
+        }
+
+        public void setDeskFree(int deskId, String userIdentity) {
+            if (deskId < 0 || deskId >= this.desks.length)
+                throw new IllegalArgumentException("Desk ID " + deskId + " does not exist");
+            if (this.desks[deskId] == null)
+                throw new RuntimeException("Desk with ID="+deskId+" is already empty.");
+            if (!this.desks[deskId].equalsIgnoreCase(userIdentity))
+                throw new IllegalArgumentException("Violation of desk freeing. Identity of applicant is different from "
+                        + "desk's current user");
+            this.desks[deskId] = null;
+            if (this.firstFree == -1 || deskId < this.firstFree)
+                this.firstFree = deskId;
+        }
+
+        /**
+         * @return <code>-1</code> if there was no free desk to occupy or <code>applicantIdentity</code> not provided,
+         * else <code>ID</code> of assigned desk (strategy of assigning is an internal logic).
+         */
+        public int occupyDesk(String applicantIdentity) {
+            if (applicantIdentity == null || applicantIdentity.isBlank() || this.firstFree == -1)
+                return -1;
+            int assigned = this.firstFree;
+            this.desks[assigned] = applicantIdentity;
+
+            this.firstFree = -1;
+            for (int i = assigned; i < this.desks.length; i++) {
+                if (this.desks[i] == null) {
+                    this.firstFree = i;
+                    break;
+                }
+            }
+            return assigned;
+        }
+
+        @Override
+        public String toString() {
+            return "DeskAllocation{" +
+                    "firstFree=" + firstFree +
+                    ", desks=" + Arrays.toString(desks) +
+                    '}';
+        }
+
+        public void freeAllDesks() {
+            Arrays.fill(this.desks, null);
+            this.firstFree = 0;
+        }
     }
 
     public static void main(String[] args) {
 
-        Queue<Integer> fifo = new ConcurrentLinkedQueue<>();
-        fifo.add(5);
-        System.out.println(fifo);
-        fifo.add(18);
-        System.out.println(fifo);
-        fifo.add(1);
-        System.out.println(fifo);
-        fifo.add(8);
-        System.out.println(fifo);
-        fifo.add(7);
-        System.out.println(fifo);
-
-        System.out.println("\n Polling..");
-        final int l = fifo.size();
-        for (int i = 0; i < l; i++) {
-            Integer polled = fifo.poll();
-            System.out.println("polled: " + polled);
-        }
     }
 }
